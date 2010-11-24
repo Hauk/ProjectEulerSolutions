834f6ae2888727cfaa30e94f1c230039dafe7f3b
Eoghan Lappin <eoghanlappin@gmail.com>
2010-11-24 01:43:02 +0000

    Updated Solution 7 + tidied + commented code.

--- problem7.java
+++ problem7.java
@@ -11,35 +11,41 @@ class ThousandPrimes
 		return true;
 	}
 	
-	static int calcThousanthPrime()
+	//Function to generate the 10001st prime number.
+	static void calcThousanthPrime()
 	{
 		boolean endCalcPrime = false;
 		
+		//Init our tracker and iterator.
 		int primeTracker = 0;
 		int lastPrime = 0;
 		int i = 2;
 		
+		//While the primeTracker is less than 10,000.
 		while(endCalcPrime == false)
 		{			
+			//If it's a prime number and i is an odd number
 			if((checkPrime(i) == true) && (i % 2 != 0))
 			{
 				lastPrime = i;
 				primeTracker++;
 			}
 			
+			//If we have reached the 10,001st number: Finish.
 			if(primeTracker == 10000)
 				endCalcPrime = true;
 			
+			//Increment our tick.
 			i++;
 		}
 		
 		System.out.println("The 1001st prime is: " + lastPrime + ".");
-		
-		return lastPrime;
 	}
 	
 	public static void main(String [] args)
 	{
+		//A program to calculate the 10,001st prime number.
+		
 		calcThousanthPrime();
 	}
 }
