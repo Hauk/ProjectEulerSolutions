007ec3f52dfb785a073b163a2be3fa53c8402090
Eoghan Lappin <eoghanlappin@gmail.com>
2010-11-25 22:22:16 +0000

    Adding Solution to Problem 5.

--- problem5.java
+++ problem5.java
@@ -4,6 +4,7 @@ class EvenlyDivide
 {
 	static int smallestDiv()
 	{
+		//Track when a number which is evenly divisible is found.
 		boolean divisibleEvenly = true;
 		
 		int trackCount = 0;
@@ -11,34 +12,38 @@ class EvenlyDivide
 		
 		while(divisibleEvenly)
 		{
+			//If the numberRange is evenly divisible.
 			for(int i = 20; i > 0; i--)
 			{
+				//If number range divided by i is even.
 				if(numRange % i == 0)
 				{
+					//Increase count.
 					trackCount++;
 				}
 			}
-				if(trackCount == 20)
-				{
-					divisibleEvenly = false;
-					break;
-				}
-				else
-				{
-					trackCount = 0;
-				}
+			
+			//If the count is == 20. It's not divisible by all values, so break.
+			if(trackCount == 20)
+			{
+				divisibleEvenly = false;
+				break;
+			}
+			else
+			{
+				//Reset the count.
+				trackCount = 0;
+			}
+			//Increase the number to check by 20(i.e. the value must divide by 20 evenly, so increasing by it makes sense).
 			numRange = numRange + 20;	
 		}
-		
+		//And return.
 		return numRange;
 	}
 	
 	public static void main(String [] args)
 	{
 		//Program to calculate the smallest divisible number by all nums from 1 - 20.
-		
-		int answer = smallestDiv();
-		
-		System.out.println(answer);
+		System.out.println(smallestDiv());
 	}
 }
